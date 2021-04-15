58abf41754bbe76060611bf2cf82a5c877a059a8
RACAUD Simon <simon.racaud@epitech.eu>
2021-04-15 16:10:46 +0200

    feat(account): account list init and destroy

--- src/app/account.c
+++ src/app/account.c
@@ -6,8 +6,10 @@
 */
 
 #include "account_t.h"
+#include "app_t.h"
+#include <string.h>
 
-const account_t ACCOUNT[] = {
+static const account_t ACCOUNT[] = {
     {
         .username = "ANONYMOUS",
         .password = "",
@@ -19,3 +21,31 @@ const account_t ACCOUNT[] = {
         .home_path = NULL
     }
 };
+
+int account_init(app_t *app, const char *default_path)
+{
+    size_t nb_account = 0;
+
+    for (; ACCOUNT[nb_account].username != NULL; nb_account++);
+    app->account_list = malloc(sizeof(account_t *) * (nb_account + 1));
+    if (!app->account_list)
+        return EXIT_FAILURE;
+    app->account_list[nb_account] = NULL;
+    for (size_t i = 0; i < nb_account; i++) {
+        app->account_list[i] = malloc(sizeof(account_t));
+        if (!app->account_list[i])
+            return EXIT_FAILURE;
+        app->account_list[i]->username = ACCOUNT[i].username;
+        app->account_list[i]->password = ACCOUNT[i].password;
+        app->account_list[i]->home_path = strdup(default_path);
+    }
+    return EXIT_SUCCESS;
+}
+
+void account_destroy(app_t *app) {
+    for (size_t i = 0; app->account_list[i]; i++) {
+        free(app->account_list[i]->home_path);
+        free(app->account_list[i]);
+    }
+    free(app->account_list);
+}
