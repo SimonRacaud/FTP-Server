9640e02c618f4cb6cb0849c789d7d0051ec53b45
RACAUD Simon <simon.racaud@epitech.eu>
2021-04-17 16:14:05 +0200

    feat(server): server_connect_client

---
+++ src/server/server_connect_client.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myftp-simon.racaud
+** File description:
+** 17/04/2021 server_connect_client.c
+*/
+
+#include "server.h"
+
+int server_connect_client(server_t *server)
+{
+    connection_t *client = connection_create(server->sock.fd);
+
+    if (!client)
+        return EXIT_FAILURE;
+    if (connection_list_push(client, &server->clients) == EXIT_FAILURE)
+        return EXIT_FAILURE;
+    server->select.status--;
+    return EXIT_SUCCESS;
+}
