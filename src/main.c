8b9d86465d07a1ebb67483e57c574898a28dd094
RACAUD Simon <simon.racaud@epitech.eu>
2021-04-17 16:18:56 +0200

    feat(main): main function

--- src/main.c
+++ src/main.c
@@ -7,8 +7,23 @@
 
 #include "app.h"
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
+    args_t parsed_args;
+    app_t app;
+    const uint max_client = 100;
 
+    if (arg_parser(argc, argv, &parsed_args) == EXIT_FAILURE) {
+        fprintf(stderr, "%s: Invalid parameters\n\n", argv[0]);
+        return usage(EXIT_FAILURE, argv[0]);
+    }
+    if (app_init(&app, &parsed_args, max_client) == EXIT_FAILURE)
+        return EXIT_FAILURE;
+    if (signal_init(&app) == EXIT_FAILURE)
+        return EXIT_FAILURE;
+    if (app_loop(&app) == EXIT_FAILURE)
+        return EXIT_FAILURE;
+    if (app_destroy(&app) == EXIT_FAILURE)
+        return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
