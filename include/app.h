06a874c37955fbae99f42e96a53f03eb85d6f44d
RACAUD Simon <simon.racaud@epitech.eu>
2021-04-17 16:18:31 +0200

    feat(app): app header, add functions prototypes

--- include/app.h
+++ include/app.h
@@ -12,7 +12,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <string.h>
 
 #include "app_t.h"
+#include "args_t.h"
+
+#ifdef EXIT_FAILURE
+    #undef EXIT_FAILURE
+#endif
+#define EXIT_FAILURE 84
+
+// Core
+int arg_parser(int argc, char **argv, args_t *dest);
+int signal_init(app_t *app);
+
+int app_init(app_t *app, args_t *args, uint max_client);
+int app_loop(app_t *app);
+int app_destroy(app_t *app);
+
+int account_init(app_t *app, const char *default_path);
+void account_destroy(app_t *app);
+
+int usage(int status, const char *bin);
 
 #endif /* !APP_H_ */
